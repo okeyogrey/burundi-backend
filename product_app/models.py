19a356946500b054e2658daad1b58702500b4ca3
okeyogrey <greyokeyo@gmail.com>
2025-03-13 22:06:05 +0300

    Fixed Product model import issue

--- product_app/models.py
+++ product_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_on_sale = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
