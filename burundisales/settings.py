9b638040a5ebb91555e48254799e3c72a2875dcc
okeyogrey <greyokeyo@gmail.com>
2025-05-23 12:24:26 +0300

    database import

--- burundisales/settings.py
+++ burundisales/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+import dj_database_url
+
 
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
